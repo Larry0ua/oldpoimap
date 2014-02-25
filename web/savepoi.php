ff3debf7eb7ccc7abcf919e35c6309ebfa946a8c
Larry0ua <pekarskiy.valeriy@gmail.com>
2014-02-26 00:22:40 +0200

    Move to postgresql

--- web/savepoi.php
+++ web/savepoi.php
@@ -16,8 +16,8 @@ $data = json_decode($data, true);
 //var_dump($data);
 if(!$data) die('No data provided');
 
-$c = new mysqli('localhost', DB_USER, DB_PASS, DB_NAME);
-if($c->connect_errno) { die("Db connect failed");}
+$c = new PDO('pgsql:dbname='.DB_NAME.';host=localhost;port=5432', DB_USER, DB_PASS);
+$utc = new DateTimeZone('UTC');
 
 // save from client to db
 $pois_to_save = array();
@@ -28,74 +28,57 @@ $updated = 0;
 $error_str = '';
 if ($action=='put' && count($data)>0) {
 	$stmt_sel = $c->prepare('select osm_version, check_date from pois_inspection where osm_id = ?');
-	$stmt_sel->bind_param("i", $poi_id);
-	$stmt_sel->bind_result($result_version, $result_check_date);
-//	echo $c->error;
 	foreach ($data as $poi) {
-		$poi_id = $poi['id'];
-		$stmt_sel->execute();
-		$ftch = $stmt_sel->fetch();
-		//var_dump($ftch);
-		if ($ftch === NULL) { 
+		$stmt_sel->execute(array($poi['id']));
+		$ftch = $stmt_sel->fetch(PDO::FETCH_NUM);
+		
+		if ($ftch === FALSE) { 
 		    $pois_to_save[] = $poi;
-		    //echo "<br>will add poi $poi_id";
-		} elseif ($ftch === TRUE) {
-		    if ($result_version != $poi['version']) {
+//		    echo "<br>will add poi";
+		} else {
+		    if ($ftch[0] != $poi['version']) {
 		        $pois_to_update[] = $poi;
-    		    //echo "<br>will update poi $poi_id";
+//    		    echo "<br>will update poi in database";
 		    } 
-		    if ($result_check_date > $poi['timestamp']) {
-		        $pois_to_return[$poi['id']]=$result_check_date;
+			
+			$date_from_db = strtotime($ftch[1]);
+			$date_from_client = strtotime($poi['timestamp']);
+		    if ($date_from_db > $date_from_client) {
+		        $pois_to_return[(string)$poi['id']]=$ftch[1];
+//				echo "<br>will return newer poi ".$poi['id'];
 		    }
 		}
 	}
-	$stmt_sel->close();
+
 	if (count($pois_to_save)>0) {
-    	$stmt_put = $c->prepare('insert into pois_inspection (osm_id, osm_type, osm_version, centroid, check_date) values (?, 0, ?, GeomFromText(?, 4326), ?)');
-    	$stmt_put->bind_param("iiss", $poi_id, $poi_version, $point_coords, $poi_timestamp);
+    	$stmt_put = $c->prepare('insert into pois_inspection (osm_id, osm_type, osm_version, centroid, check_date) values (?, 0, ?, point(?, ?), ?)');
     	foreach ($pois_to_save as $poi) {
-    		$poi_id = $poi['id'];
-    		$poi_version = $poi['version'];
-            $point_coords = 'POINT('.floatval($poi['lon']).' '.floatval($poi['lat']).')';
-            $poi_timestamp = $poi['timestamp'];
-    		$success = $stmt_put->execute();
+    		$success = $stmt_put->execute(array($poi['id'], $poi['version'], $poi['lon'], $poi['lat'], $poi['timestamp']));
     		if ($success) $added++;
-    		//echo "<br>added $poi_id(".$stmt_put->affected_rows.':'.$stmt_put->error.':'.$success.')';
+			print_r($stmt_put->errorInfo());
+//    		echo "<br>add:$success";
     	}
-    	$stmt_put->close();
 	}
 	if (count($pois_to_update)>0) {
-    	$stmt_put = $c->prepare('update pois_inspection set osm_version = ?, centroid = GeomFromText(?, 4326), check_date = ? where osm_id = ?');
-    	$stmt_put->bind_param("issi", $poi_version, $point_coords, $poi_timestamp, $poi_id);
+    	$stmt_put = $c->prepare('update pois_inspection set osm_version = ?, centroid = point(?, ?), check_date = ? where osm_id = ?');
     	foreach ($pois_to_update as $poi) {
-    		$poi_id = $poi['id'];
-    		$poi_version = $poi['version'];
-            $point_coords = 'POINT('.floatval($poi['lon']).' '.floatval($poi['lat']).')';
-            $poi_timestamp = $poi['timestamp'];
-    		$success = $stmt_put->execute();
+    		$success = $stmt_put->execute(array($poi['id'], $poi['version'], $poi['lon'], $poi['lat'], $poi['timestamp']));
     		if ($success) $updated++;
     		//echo "<br>updated $poi_id(".$stmt_put->affected_rows.':'.$stmt_put->error.':'.$success.')';
     	}
-    	$stmt_put->close();
 	}
     echo json_encode(array("updated"=>$updated, "added"=>$added, "sent"=>count($data), "newer"=>$pois_to_return));
 }
 elseif($action=='update' && $data) {
     $stmt_sel = $c->prepare('select check_date from pois_inspection where osm_id = ?');
-    $stmt_sel->bind_param("i", $poi_id);
-    $stmt_sel->bind_result($result_check_date);
-    $poi_id=$data['id'];
-    $stmt_sel->execute();
+    $stmt_sel->execute(array($data['id']));
     $ftch=$stmt_sel->fetch();
-    $stmt_sel->close();
-    if($ftch === NULL) {
+    if($ftch === FALSE) {
         $error_str = 'Not found yet, put data first';
-    } elseif ($ftch === TRUE) {
-        $stmt_upd = $c->prepare('update pois_inspection set check_date=now() where osm_id = ?');
-        $stmt_upd->bind_param("i", $poi_id);
-        $success = $stmt_upd->execute();
+    } else {
+        $stmt_upd = $c->prepare('update pois_inspection set check_date=CURRENT_TIMESTAMP where osm_id = ?');
+        $success = $stmt_upd->execute(array($data['id']));
         if ($success) $updated++;
-        $stmt_upd->close();
     }
     echo json_encode(array("updated"=>$updated, "message"=>$error_str));
 }
