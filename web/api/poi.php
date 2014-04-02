6cbc714a660dfc7fb906a3e9050a126aee4547eb
Larry0ua <pekarskiy.valeriy@gmail.com>
2014-04-03 00:00:26 +0300

    update rest service for points

--- web/api/poi.php
+++ web/api/poi.php
@@ -5,22 +5,29 @@ require '../config.php';
 
 $request = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
+/*
+get: /api/poi/123 - returns poi {id:123, updated:'date'} or 404 if not set
+get: /api/poi/?1,2,3,4 - returns [{id:123, updated:'date'},]
+post: /api/poi/ - add 123 as updated now, body: {id:123, lat:1, lng: 1}
+put: - no need to update in db
+delete: /api/poi/123 - remove from database as osm has a newer version. server-side check?
+*/
 
-if (preg_match("/\/api\/poi\/(\\d+)(\/[nwr])?$/", $request, $result)) {
+if ($method == 'GET' && preg_match("/\/api\/poi\/(\\d+)(\/[nwr])?$/", $request, $result)) {
 	// one POI request
 	$osm_id = $result[1];
 	// skip $2 as not supported by us yet
-	$c = new PDO('pgsql:dbname='.DB_NAME.';host=localhost;port=5432', DB_USER, DB_PASS);
-	$c->prepare('select * from pois_inspection where osm_id = ?');
-	$c->execute(array($osm_id));
-	$row = $c->fetch(PDO::FETCH_ASSOC);
+	$c = db_connect();
+	$stmt = $c->prepare('select * from pois_inspection where osm_id = ?');
+	$stmt->execute(array($osm_id));
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
 	if ($row === FALSE) {
 		header("HTTP/1.0 404 Not Found");
+		die();
 	} else {
-		echo json_encode($row);
+		echo json_encode(array('id'=>$row['osm_id'], 'updated'=>$row['check_date']));
 	}
-	die("get point with osm_id=$result[1]");
-} else if (preg_match("/\/api\/poi\/\?([0-9.,]+)$/", $request, $result)) {
+} else if ($method == 'GET' && preg_match("/\/api\/poi\/\?([0-9.,]+)$/", $request, $result)) {
 	$bbox = $result[1];
 	$coords = explode(',', $bbox); // x1, y1, x2, y2
 	if (count($coords) != 4) {
@@ -34,54 +41,41 @@ if (preg_match("/\/api\/poi\/(\\d+)(\/[nwr])?$/", $request, $result)) {
 
 	$s = abs(($x1-$x2)*($y1-$y2));
 	if ($s > MAX_SQ || $s == 0) {
-		header("HTTP/1.0 509 Bad area selected");
+		header("HTTP/1.0 409 Bad area selected");
+		die();
 	}
 
-	$c = new PDO('pgsql:dbname='.DB_NAME.';host=localhost;port=5432', DB_USER, DB_PASS);
-	$c->prepare('select * from pois_inspection where centroid <@ box((?, ?), (?, ?)');
-	$c->execute(array($x1, $y1, $x2, $y2));
-	$row = $c->fetch(PDO::FETCH_ASSOC);
-	if ($row === FALSE) {
-		header("HTTP/1.0 404 Not Found");
+	$c = db_connect();
+	$stmt = $c->prepare('select * from pois_inspection where lng between symmetric ? and ? and lat between symmetric ? and ?');
+	$stmt->execute(array($x1, $x2, $y1, $y2));
+	$result = array();
+	while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+		$result[] = array('id'=>$row['osm_id'], 'updated'=>$row['check_date']);
+	}
+	echo json_encode($result);
+} elseif ($method == 'POST' && preg_match('/\/api\/poi\/$/', $request)) {
+	$object = json_decode(file_get_contents("php://input"), true);
+	$osm_id = $object['id'];
+	if (intval($osm_id) != $osm_id || $osm_id <= 0) {
+		header("HTTP/1.0 409 Bad Request");
+		die();
+	}
+	$c = db_connect();
+	$stmt = $c->prepare('select count(*) from pois_inspection where osm_id = ?');
+	$stmt->execute(array($osm_id));
+	$row = $stmt->fetch(PDO::FETCH_NUM);
+	if ($row[0] == 1) {
+		$stmt = $c->prepare('update pois_inspection set check_date=CURRENT_TIMESTAMP where osm_id = ?');
+		$stmt->execute(array($osm_id));
 	} else {
-		echo json_encode($row);
+		$stmt = $c->prepare('insert into pois_inspection (osm_id, check_date, centroid) values (?, CURRENT_TIMESTAMP, point(?, ?))');
+		$stmt->execute(array($osm_id, $object['lat'], $object['lng']));
 	}
-
-	die("find by bbox $coords[0] $coords[1]");
+} elseif ($method == 'DELETE' && preg_match("/\/api\/poi\/(\\d+)(\/[nwr])?$/", $request, $result)) {
+	$c = db_connect();
+	$stmt = $c->prepare('delete from pois_inspection where osm_id = ?');
+	$stmt->execute(array($result[1]));
 } else {
+	header("HTTP/1.0 409 Bad Request");
 	die("Unrecognized path $request with method $method");
 }
-
-/*$data = $_REQUEST['data'];
-$action = $_REQUEST['action'];
-$data = json_decode($data, true);
-
-//var_dump($data);
-if(!$data) die('No data provided');
-
-$c = new PDO('pgsql:dbname='.DB_NAME.';host=localhost;port=5432', DB_USER, DB_PASS);
-*/
-/*if ($action=='put' && count($data)>0) {
-	$stmt_sel = $c->prepare('select osm_version, check_date from pois_inspection where osm_id = ?');
-	foreach ($data as $poi) {
-		$stmt_sel->execute(array($poi['id']));
-		$ftch = $stmt_sel->fetch(PDO::FETCH_NUM);
-		
-		if ($ftch === FALSE) { 
-		    $pois_to_save[] = $poi;
-//		    echo "<br>will add poi";
-		} else {
-		    if ($ftch[0] != $poi['version']) {
-		        $pois_to_update[] = $poi;
-//    		    echo "<br>will update poi in database";
-		    } 
-			
-			$date_from_db = strtotime($ftch[1]);
-			$date_from_client = strtotime($poi['timestamp']);
-		    if ($date_from_db > $date_from_client) {
-		        $pois_to_return[(string)$poi['id']]=$ftch[1];
-//				echo "<br>will return newer poi ".$poi['id'];
-		    }
-		}
-	}
-*/
--- web/config.php
+++ web/config.php
@@ -3,3 +3,7 @@ error_reporting(E_ALL);
 define('DB_USER', 'poiuser');
 define('DB_PASS', 'poiuser');
 define('DB_NAME', 'pois');
+define('MAX_SQ', 0.05);
+function db_connect() {
+    return new PDO('pgsql:dbname='.DB_NAME.';host=localhost;port=5432', DB_USER, DB_PASS);
+}
